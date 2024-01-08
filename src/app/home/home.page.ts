62dab627f5065ffd1028028924835dc01776ce54
Nagy János <janos@zold.lan>
2024-01-08 12:26:56 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BaseService } from '../base.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  messages:any
+  newMessage:any
+  userName="Attila"
+  constructor(private base:BaseService) {
+    this.base.getMessages().snapshotChanges()
+    .pipe(
+      map( (ch)=> ch.map(
+        (c)=>({ key:c.payload.key, ...c.payload.val()})
+      ))
+    )
+    .subscribe((res)=>this.messages=res)
+  }
 
-  constructor() {}
+  addMessage(){
+    let body={
+      user:this.userName, 
+      time:new Date().toLocaleTimeString(), 
+      content:this.newMessage }
+    this.base.addMessage(body)
+    this.newMessage=""
 
+
+  }
+  deleteMessage(body:any){
+    this.base.deleteMessage(body)
+  }
 }
